db715edb17f8a49767635c8e8afceaf6e9007203
c p <1107223894@qq.com>
2018-10-23 23:11:19 +0800

    处理carthage报错问题

---
+++ ArekDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArekDemo
+//
+//  Created by chenp on 2018/10/23.
+//  Copyright © 2018 chenp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
